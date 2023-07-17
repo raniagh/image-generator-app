f13525d97a22d0e8b0d02fd280ad442ae58755f7
Your Name <rghzaiel@ggn-group.com>
2023-07-17 15:16:28 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,87 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
 export default function Home() {
+  const [result, setResult] = useState(
+    "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+  );
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [typedText, setTypedText] = useState("");
+  const text = "Creating image...Please Wait...";
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  if (!apiKey) {
+    throw new Error("apiKey is not defined in config file");
+  }
+  const configuration = new Configuration({ apiKey });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    setLoading(true);
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setLoading(false);
+    const data = res.data;
+    setResult(data.data[0].url || "no image found");
+  };
+
+  useEffect(() => {
+    if (loading) {
+      let i = 0;
+      const typing = setInterval(() => {
+        setTypedText(text.slice(0, i));
+        i++;
+        if (i > text.length + 1) {
+          i = 0;
+          setTypedText("");
+        }
+      }, 100);
+      return () => clearInterval(typing);
+    }
+    // write some logic here
+  }, [loading]);
+
+  const sendEmail = (url = "") => {
+    url = result;
+    const message = `Here's your image download link: ${url}`;
+    window.location.href = `mailto:someone@example.com?subject=Image Download Link&body=${message}`;
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="app-main">
+      <h2>Create Images With Your Imagination</h2>
+      <textarea
+        className="app-input"
+        placeholder="Create any type of image you can think of with 
+as much added description as you would like"
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button onClick={generateImage}>Generate Image</button>
+      <>
+        {loading ? (
+          <>
+            <h3>{typedText}</h3>
+            <div className="lds-ripple">
+              <div></div>
+              <div></div>
+            </div>
+          </>
+        ) : (
+          <img
+            onClick={() => sendEmail(result)}
+            src={result}
+            style={{ cursor: "pointer" }}
+            className="result-image"
+            alt="result"
+          />
+        )}
+      </>
+    </div>
+  );
 }
